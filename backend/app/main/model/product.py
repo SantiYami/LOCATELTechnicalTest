d0a26afa0d18737406c50127a9e19a5ed665d703
SantiYami <davidgarces.98@gmail.com>
2024-08-27 22:32:16 -0500

    Update removed numeric for sqlite

--- backend/app/main/model/product.py
+++ backend/app/main/model/product.py
@@ -7,6 +7,6 @@ class Product(db.Model):
     id_product = db.Column(db.Integer, primary_key=True, autoincrement=True, info={'comment': 'Identificador único del producto'})
     code = db.Column(db.String(30), nullable=False, unique=True, info={'comment': 'Código del producto'})
     name = db.Column(db.String(100), nullable=False, info={'comment': 'Nombre del producto'})
-    sale_value = db.Column(db.Numeric(10, 2), nullable=False, info={'comment': 'Valor de venta del producto'})
+    sale_value = db.Column(db.String(30), nullable=False, info={'comment': 'Valor de venta del producto'})
     handles_iva = db.Column(db.Boolean, nullable=False, default=False, info={'comment': 'Indicador si el producto maneja IVA'})
-    iva_percentage = db.Column(db.Numeric(5, 2), nullable=True, info={'comment': 'Porcentaje de IVA aplicable si maneja IVA'})
+    iva_percentage = db.Column(db.String(4), nullable=True, info={'comment': 'Porcentaje de IVA aplicable si maneja IVA'})
--- backend/app/main/model/sale.py
+++ backend/app/main/model/sale.py
@@ -9,7 +9,7 @@ class Sale(db.Model):
     consecutive = db.Column(db.String(50), nullable=False, unique=True, info={'comment': 'Número consecutivo de la venta'})
     date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False, info={'comment': 'Fecha de la venta'})
     user_id = db.Column(db.Integer, db.ForeignKey('user.id_user'), nullable=False, info={'comment': 'Identificador del usuario que realizó la compra'})
-    total_sale = db.Column(db.Numeric(10, 2), nullable=False, info={'comment': 'Monto total de la venta'})
+    total_sale = db.Column(db.String(30), nullable=False, info={'comment': 'Monto total de la venta'})
 
     # Definición de relación con la tabla User
     user = db.relationship('User', backref=db.backref('sales', lazy=True))
