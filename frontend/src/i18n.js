257ba56b3b3f1daf06418e6fa1cb2213b570fd78
SantiYami <davidgarces.98@gmail.com>
2024-08-27 23:47:34 -0500

    Update i18n.js

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -2,17 +2,11 @@ const messages = {
     en: {
     },
     es: {
-        inventory:{
-            products: 'Productos',
-            name: 'Nombre de producto',
-            quantity: 'Cantidad',
-            price: 'Precio',
-            supplier: 'Proveedor',
-            action: 'Acciones',
-            edit: 'Editar Producto',
-            delete: 'Eliminar Producto',
-            add: 'Crear nuevo producto',
-            save: 'Guardar'
+        login:{
+            title: 'Login',
+            email: 'Correo',
+            password: 'Contraseña',
+            login: 'Ingresar',
         },
     }
 }
