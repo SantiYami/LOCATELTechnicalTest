a0acf3003d46ec894a8bdaafcf27f9208a852f4b
SantiYami <davidgarces.98@gmail.com>
2024-08-27 23:17:39 -0500

    Add toastification

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,7 +14,27 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
 import '@/stylessheets/style.scss'
+
+//* TOAST OPTIONS
+const options = {
+  position: "bottom-center",
+  timeout: 5000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  closeButton: "button",
+  icon: true,
+  rtl: false
+};
+
+Vue.use(Toast, options);
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
